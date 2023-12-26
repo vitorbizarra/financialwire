1662f79629c4680fa8158e345ad06d24aec6c01d
vitorbizarra <vitorbizarracontato@gmail.com>
2023-12-25 23:37:53 -0300

    :lipstick: feat: Adding HasColor interface to TransactionType enum

--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -2,10 +2,12 @@
 
 namespace App\Enums;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
 
-enum TransactionType: string implements HasLabel, HasIcon
+enum TransactionType: string implements HasLabel, HasIcon, HasColor
 {
     case Expense = 'expense';
     case Income = 'income';
@@ -23,6 +25,14 @@ enum TransactionType: string implements HasLabel, HasIcon
         return match ($this) {
             self::Expense => 'heroicon-m-arrow-trending-down',
             self::Income => 'heroicon-m-arrow-trending-up'
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Expense => Color::Red,
+            self::Income => Color::Lime
         };
     }
 }
