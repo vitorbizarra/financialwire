b3e67e319709f062c98bb1ce002c4f001dd41f77
vitorbizarra <vitorbizarracontato@gmail.com>
2023-12-30 16:02:58 -0300

    :lipstick: feat: Changing primary color from Panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->profile(Pages\Auth\EditProfile::class)
             ->topNavigation()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Purple,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
