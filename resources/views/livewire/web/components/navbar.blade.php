97e8085af7f3c0bd89d15ab9077e82985bbda36c
vitorbizarra <vitorbizarracontato@gmail.com>
2024-01-09 23:16:54 -0300

    :bug: fix: Adding route to panel button on navbar

--- resources/views/livewire/web/components/navbar.blade.php
+++ resources/views/livewire/web/components/navbar.blade.php
@@ -1,15 +1,16 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ env('APP_NAME')
-                }}</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                {{ env('APP_NAME')}}
+            </span>
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             @auth
-            <button type="button"
+            <a href="{{ route('filament.app.pages.dashboard') }}"
                 class="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800 transition-all ease-in-out">
                 Painel
-            </button>
+            </a>
             @endauth
 
             @guest
