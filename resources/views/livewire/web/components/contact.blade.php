82af1669467c46d90a2f6393501295ca97cbc6fa
vitorbizarra <vitorbizarracontato@gmail.com>
2024-01-18 22:46:42 -0300

    :construction: wip: Contact section

--- resources/views/livewire/web/components/contact.blade.php
+++ resources/views/livewire/web/components/contact.blade.php
@@ -1,34 +1,44 @@
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us
+<section class="container mx-auto grid grid-cols-1 md:grid-cols-2 px-4 py-8 md:py-16 gap-8 md:gap-16" id="contact">
+    <div class="max-sm:text-center">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+            Entre em Contato
         </h2>
-        <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical
-            issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-        <form action="#" class="space-y-8">
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-                    email</label>
-                <input type="email" id="email"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="name@flowbite.com" required>
-            </div>
-            <div>
-                <label for="subject"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                <input type="text" id="subject"
-                    class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="Let us know how we can help you" required>
-            </div>
-            <div class="sm:col-span-2">
-                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
-                    message</label>
-                <textarea id="message" rows="6"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Leave a comment..."></textarea>
-            </div>
-            <button type="submit"
-                class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
-                message</button>
-        </form>
+        <p class="font-light text-gray-500 sm:text-xl">
+            Encontrou algum problema técnico? Gostaria de nos dar um feedback? Entre em contato!
+            Estamos aqui para ajudar e receber suas sugestões.
+        </p>
     </div>
+
+    <form action="#" class="space-y-8">
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">
+                Seu melhor email:
+            </label>
+            <input type="email"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5"
+                placeholder="eu@financialwire.com.br">
+        </div>
+
+        <div>
+            <label for="subject" class="block mb-2 text-sm font-medium text-gray-900">
+                Assunto:
+            </label>
+            <input type="text"
+                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-violet-500 focus:border-violet-500"
+                placeholder="Motivo do contato">
+        </div>
+
+        <div class="sm:col-span-2">
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900">
+                Mensagem:
+            </label>
+            <textarea rows="6"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-violet-500 focus:border-violet-500"
+                placeholder="Sua mensagem..."></textarea>
+        </div>
+        <button type="submit"
+            class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-violet-700 sm:w-fit hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300">
+            Enviar
+        </button>
+    </form>
 </section>
