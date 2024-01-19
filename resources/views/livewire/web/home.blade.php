ae2fc3e2e7938a51e46f98a6ec94053174d0d340
vitorbizarra <vitorbizarracontato@gmail.com>
2024-01-18 22:08:19 -0300

    :fire: Deleting About component

--- resources/views/livewire/web/home.blade.php
+++ resources/views/livewire/web/home.blade.php
@@ -1,8 +1,6 @@
 <div class="container mx-auto">
     <livewire:web.components.hero />
 
-    <livewire:web.components.about />
-
     <livewire:web.components.features />
 
     <livewire:web.components.contact />
