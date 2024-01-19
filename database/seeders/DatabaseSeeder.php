9703b484b9f0a69d2b11c00351371d5bed31454f
vitorbizarra <vitorbizarracontato@gmail.com>
2024-01-18 23:40:51 -0300

    :wrench: cli: TransactionFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             AccountSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            TransactionSeeder::class,
         ]);
     }
 }
